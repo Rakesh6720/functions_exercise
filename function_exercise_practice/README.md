1f316fc403caeca533a23ebdb0614f2f5d9e718a
Rakesh <rbaruah@asusLaptop.(none)>
2018-04-09 12:02:52 -0500

    final commit

---
+++ function_exercise_practice/README.md
@@ -0,0 +1,2 @@
+# function_exercise_practice
+Practice files for my exercise to learn more about functions, parameters, and arguments
---
+++ function_exercise_practice/function_exercise_practice.js
@@ -0,0 +1,37 @@
+
+/*function addTwoNumbers(){
+	let numberOne=document.getElementById("apples").value;
+	let numberTwo=document.getElementById("oranges").value;
+	let a = Number(numberOne);
+	let b = Number(numberTwo);
+	
+	let sum = a + b;
+	console.log(sum);
+}
+*/
+function calculatePay(){
+}
+	
+function getValues(){
+	let hoursInputted=document.getElementById("hourInput").value;
+	let payInputted=document.getElementById("payInput").value;
+	return hoursInputted, payInputted;
+
+	
+function convertValuesToNumbers(){
+	let numberHour=Number(hoursInputted);
+	let numberPay=Number(payInputted);
+	return numberHour, numberPay;
+}
+	
+function evaluateInputs(){
+	
+}
+
+	/*let a = Number(numberOne);
+	let b = Number(numberTwo);
+	
+	let weekPay = a * b;
+	console.log(weekPay);
+}
+*/
